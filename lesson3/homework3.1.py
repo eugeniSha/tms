1e0f5129aa56ae3c484f5893bb5e1287c4eae707
eugeniSha <shewch@icloud.com>
2023-05-22 11:39:35 +0300

    lesson3

---
+++ lesson3/homework3.1.py
@@ -0,0 +1,6 @@
+dva = input('введите два слова  ').split()
+word_1, word_2 = dva[0], dva[1]
+# rez= f'!{word_2} ! {word_1}!'
+print(f'! {word_2} ! {word_1} !')
+print('! {} ! {} !'.format(word_2, word_1))
+print('! {rez1} ! {res2} !'.format(res2=word_1, rez1=word_2))

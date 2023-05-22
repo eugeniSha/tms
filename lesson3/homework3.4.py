c792ab1de11a4bd08e4c427495ec75c34485d4f6
eugeniSha <shewch@icloud.com>
2023-05-22 15:28:41 +0300

    lesson3

--- lesson3/homework3.4.py
+++ lesson3/homework3.4.py
@@ -1,17 +1,17 @@
-# n = int( input('введите целое число '))
-# start = 0
-# summ = 0
-# while start < n:
-#     qube = (start+1)**2
-#     summ = summ + qube
-#     start = start+1
-# print (summ)
-
-n = int(input('введите целое число '))
-start: int = 0
+n = int( input('введите целое число '))
+start = 0
 summ = 0
-for < n:
-    qube = (start + 1) ** 2
+while start < n:
+    qube = (start+1)**3
     summ = summ + qube
-    start = start + 1
-print(summ)
+    start = start+1
+print (summ)
+
+# n = int(input('введите целое число '))
+# start: int = 0
+# summ = 0
+# for < n:
+#     qube = (start + 1) ** 2
+#     summ = summ + qube
+#     start = start + 1
+# print(summ)
